9d01574cbddd18e5c7a377ce126e77c6b508f9bf
Raymond Chua <ray.r.chua@gmail.com>
2024-03-26 13:53:11 -0400

    Remove print statements used for debugging.

--- agent/sf_simple.py
+++ agent/sf_simple.py
@@ -11,9 +11,9 @@ import utils
 
 from agent.ddpg import DDPGAgent
 
+
 class CriticSF(nn.Module):
-    def __init__(self, obs_type, obs_dim, action_dim, feature_dim, hidden_dim,
-                 sf_dim):
+    def __init__(self, obs_type, obs_dim, action_dim, feature_dim, hidden_dim, sf_dim):
         super().__init__()
 
         self.obs_type = obs_type
@@ -22,31 +22,30 @@ class CriticSF(nn.Module):
         # whereas we directly use the trunk to get the features. Therefore, to get the
         # basis features dim to match the sf_dim, we include an additional linear layer.
 
-        if obs_type == 'pixels':
+        if obs_type == "pixels":
             # for pixels actions will be added after trunk
-            self.trunk = nn.Sequential(nn.Linear(obs_dim, feature_dim),
-                                       nn.LayerNorm(feature_dim), nn.Tanh(),
-                                       nn.Linear(hidden_dim, sf_dim))
+            self.trunk = nn.Sequential(
+                nn.Linear(obs_dim, feature_dim),
+                nn.LayerNorm(feature_dim),
+                nn.Tanh(),
+                nn.Linear(hidden_dim, sf_dim),
+            )
             trunk_dim = sf_dim + action_dim
         else:
             # for states actions come in the beginning
             self.trunk = nn.Sequential(
                 nn.Linear(obs_dim + action_dim, hidden_dim),
-                nn.LayerNorm(hidden_dim), nn.Tanh(),
-                nn.Linear(hidden_dim, sf_dim))
+                nn.LayerNorm(hidden_dim),
+                nn.Tanh(),
+                nn.Linear(hidden_dim, sf_dim),
+            )
             trunk_dim = sf_dim
 
         def make_q():
             q_layers = []
-            q_layers += [
-                nn.Linear(trunk_dim, hidden_dim),
-                nn.ReLU(inplace=True)
-            ]
-            if obs_type == 'pixels':
-                q_layers += [
-                    nn.Linear(hidden_dim, hidden_dim),
-                    nn.ReLU(inplace=True)
-                ]
+            q_layers += [nn.Linear(trunk_dim, hidden_dim), nn.ReLU(inplace=True)]
+            if obs_type == "pixels":
+                q_layers += [nn.Linear(hidden_dim, hidden_dim), nn.ReLU(inplace=True)]
             q_layers += [nn.Linear(hidden_dim, sf_dim)]
             return nn.Sequential(*q_layers)
 
@@ -56,14 +55,13 @@ class CriticSF(nn.Module):
         self.apply(utils.weight_init)
 
     def forward(self, obs, action, task):
-        inpt = obs if self.obs_type == 'pixels' else torch.cat([obs, action],
-                                                               dim=-1)
+        inpt = obs if self.obs_type == "pixels" else torch.cat([obs, action], dim=-1)
         h = self.trunk(inpt)
 
         # main difference with aps is that we design the basis features to be the normalized of h and detach it.
         basis_features = F.normalize(h, p=2, dim=-1).detach()
 
-        h = torch.cat([h, action], dim=-1) if self.obs_type == 'pixels' else h
+        h = torch.cat([h, action], dim=-1) if self.obs_type == "pixels" else h
 
         q1 = self.Q1(h)
         q2 = self.Q2(h)
@@ -73,9 +71,11 @@ class CriticSF(nn.Module):
 
         return q1, q2, basis_features
 
+
 class SFSimpleAgent(DDPGAgent):
-    def __init__(self, update_task_every_step, sf_dim, num_init_steps, lr_task,
-                 **kwargs):
+    def __init__(
+        self, update_task_every_step, sf_dim, num_init_steps, lr_task, **kwargs
+    ):
         self.sf_dim = sf_dim
         self.update_task_every_step = update_task_every_step
         self.num_init_steps = num_init_steps
@@ -89,24 +89,34 @@ class SFSimpleAgent(DDPGAgent):
         super().__init__(**kwargs)
 
         # overwrite critic with critic sf
-        self.critic = CriticSF(self.obs_type, self.obs_dim, self.action_dim,
-                               self.feature_dim, self.hidden_dim,
-                               self.sf_dim).to(self.device)
-        self.critic_target = CriticSF(self.obs_type, self.obs_dim,
-                                      self.action_dim, self.feature_dim,
-                                      self.hidden_dim,
-                                      self.sf_dim).to(self.device)
+        self.critic = CriticSF(
+            self.obs_type,
+            self.obs_dim,
+            self.action_dim,
+            self.feature_dim,
+            self.hidden_dim,
+            self.sf_dim,
+        ).to(self.device)
+        self.critic_target = CriticSF(
+            self.obs_type,
+            self.obs_dim,
+            self.action_dim,
+            self.feature_dim,
+            self.hidden_dim,
+            self.sf_dim,
+        ).to(self.device)
         self.critic_target.load_state_dict(self.critic.state_dict())
-        self.critic_opt = torch.optim.Adam(self.critic.parameters(),
-                                           lr=self.lr)
+        self.critic_opt = torch.optim.Adam(self.critic.parameters(), lr=self.lr)
 
-        self.task_params = nn.Parameter(torch.randn(self.sf_dim, requires_grad=True, device=self.device))
+        self.task_params = nn.Parameter(
+            torch.randn(self.sf_dim, requires_grad=True, device=self.device)
+        )
         self.task_opt = torch.optim.Adam([self.task_params], lr=self.lr_task)
 
         # set solved_meta to the value of the task_params
         with torch.no_grad():
             self.solved_meta = OrderedDict()
-            self.solved_meta['task'] = self.task_params.detach().cpu().numpy()
+            self.solved_meta["task"] = self.task_params.detach().cpu().numpy()
 
         self.train()
         self.critic_target.train()
@@ -115,7 +125,7 @@ class SFSimpleAgent(DDPGAgent):
         """
         Meta dimension always follows the successor feature dimension.
         """
-        return (specs.Array((self.sf_dim,), np.float32, 'task'),)
+        return (specs.Array((self.sf_dim,), np.float32, "task"),)
 
     def init_meta(self):
         if self.solved_meta is not None:
@@ -124,7 +134,7 @@ class SFSimpleAgent(DDPGAgent):
         task = task / torch.norm(task)
         task = task.cpu().numpy()
         meta = OrderedDict()
-        meta['task'] = task
+        meta["task"] = task
         return meta
 
     def update(self, replay_iter, step):
@@ -136,14 +146,15 @@ class SFSimpleAgent(DDPGAgent):
         batch = next(replay_iter)
 
         obs, action, reward, discount, next_obs, task = utils.to_torch(
-            batch, self.device)
+            batch, self.device
+        )
 
         # augment and encode
         obs = self.aug_and_encode(obs)
         next_obs = self.aug_and_encode(next_obs)
 
         if self.use_tb or self.use_wandb:
-            metrics['batch_reward'] = reward.mean().item()
+            metrics["batch_reward"] = reward.mean().item()
 
         if not self.update_encoder:
             obs = obs.detach()
@@ -154,19 +165,24 @@ class SFSimpleAgent(DDPGAgent):
         next_obs = torch.cat([next_obs, task], dim=1)
 
         # update meta
-        metrics.update(self.regress_meta_grad_descent(next_obs.detach(), task, reward, step))
+        metrics.update(
+            self.regress_meta_grad_descent(next_obs.detach(), task, reward, step)
+        )
 
         # update critic
         metrics.update(
-            self.update_critic(obs.detach(), action, reward, discount,
-                               next_obs.detach(), task, step))
+            self.update_critic(
+                obs.detach(), action, reward, discount, next_obs.detach(), task, step
+            )
+        )
 
         # update actor
         metrics.update(self.update_actor(obs.detach(), task, step))
 
         # update critic target
-        utils.soft_update_params(self.critic, self.critic_target,
-                                 self.critic_target_tau)
+        utils.soft_update_params(
+            self.critic, self.critic_target, self.critic_target_tau
+        )
 
         return metrics
 
@@ -183,13 +199,9 @@ class SFSimpleAgent(DDPGAgent):
             next_action = dist.sample(clip=self.stddev_clip)
             next_rep = self.critic(next_obs, next_action, task)[-1]
 
-        # print which device is next_rep is on
-        print(next_rep.device)
-        print(self.task_params.device)
-
-
-
-        predicted_reward = torch.einsum("bi,i->b", next_rep, self.task_params).reshape(-1, 1)
+        predicted_reward = torch.einsum("bi,i->b", next_rep, self.task_params).reshape(
+            -1, 1
+        )
         reward_prediction_loss = F.mse_loss(predicted_reward, reward)
 
         self.task_opt.zero_grad(set_to_none=True)
@@ -202,11 +214,11 @@ class SFSimpleAgent(DDPGAgent):
             # normalize solved meta using l2 norm
             meta = meta / np.linalg.norm(meta)
             self.solved_meta = OrderedDict()
-            self.solved_meta['task'] = meta
+            self.solved_meta["task"] = meta
 
         if self.use_tb or self.use_wandb:
-            metrics['reward_prediction_loss'] = reward_prediction_loss.item()
-            metrics['task_grad_norm'] = self.task_params.grad.norm().item()
+            metrics["reward_prediction_loss"] = reward_prediction_loss.item()
+            metrics["task_grad_norm"] = self.task_params.grad.norm().item()
 
         return metrics
 
@@ -218,7 +230,9 @@ class SFSimpleAgent(DDPGAgent):
             stddev = utils.schedule(self.stddev_schedule, step)
             dist = self.actor(next_obs, stddev)
             next_action = dist.sample(clip=self.stddev_clip)
-            target_Q1, target_Q2, basis_features = self.critic_target(next_obs, next_action, task)
+            target_Q1, target_Q2, basis_features = self.critic_target(
+                next_obs, next_action, task
+            )
             target_V = torch.min(target_Q1, target_Q2)
             target_Q = reward + (discount * target_V)
 
@@ -226,10 +240,10 @@ class SFSimpleAgent(DDPGAgent):
         critic_loss = F.mse_loss(Q1, target_Q) + F.mse_loss(Q2, target_Q)
 
         if self.use_tb or self.use_wandb:
-            metrics['critic_target_q'] = target_Q.mean().item()
-            metrics['critic_q1'] = Q1.mean().item()
-            metrics['critic_q2'] = Q2.mean().item()
-            metrics['critic_loss'] = critic_loss.item()
+            metrics["critic_target_q"] = target_Q.mean().item()
+            metrics["critic_q1"] = Q1.mean().item()
+            metrics["critic_q2"] = Q2.mean().item()
+            metrics["critic_loss"] = critic_loss.item()
 
         # optimize critic
         self.critic_opt.zero_grad(set_to_none=True)
@@ -256,13 +270,12 @@ class SFSimpleAgent(DDPGAgent):
         self.actor_opt.step()
 
         if self.use_tb or self.use_wandb:
-            metrics['actor_loss'] = actor_loss.item()
-            metrics['actor_logprob'] = log_prob.mean().item()
-            metrics['actor_ent'] = dist.entropy().sum(dim=-1).mean().item()
+            metrics["actor_loss"] = actor_loss.item()
+            metrics["actor_logprob"] = log_prob.mean().item()
+            metrics["actor_ent"] = dist.entropy().sum(dim=-1).mean().item()
 
         return metrics
 
     @torch.no_grad()
     def solved_meta(self):
         return self.solved_meta
-
