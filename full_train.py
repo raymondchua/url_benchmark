a2841a10349471a69be50dda1bd7892c123e5691
Raymond Chua <ray.r.chua@gmail.com>
2024-04-12 19:22:53 -0400

    Add print statement to check action in eval.

--- full_train.py
+++ full_train.py
@@ -211,6 +211,7 @@ class Workspace:
                     action = self.agent.act(
                         time_step.observation, meta, self.global_step, eval_mode=True
                     )
+                print("action: ", action)
                 time_step = current_eval_env.step(action)
                 self.eval_video_recorder.record(current_eval_env)
                 total_reward += time_step.reward
